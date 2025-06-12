c45fa3b91f74206d04152b22e7db0cc0fda47791
Héctor Tovar <h.tovarm07@gmail.com>
2025-06-12 04:49:16 -0600

    feat: deleted the dashboard app

--- EntregaFinal/RasperryPi/dashboard/mqtt/mqttClient.ts
+++
@@ -1,9 +0,0 @@
-import mqtt from 'mqtt';
-
-const brokerUrl = 'ws://192.168.137.214:9001'; // MQTT debe estar en WebSocket (verifica que tu broker soporte esto)
-const topicPub = 'tractor/inputs';
-const topicSub = 'tractor/outputs';
-
-const client = mqtt.connect(brokerUrl);
-
-export { client, topicPub, topicSub };
